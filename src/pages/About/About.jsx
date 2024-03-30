0bfb174b1155201a02c78415acaa5c50767ba5aa
liton61 <litonofficial61@gmail.com>
2024-03-30 14:11:52 +0600

    about banner added

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,17 @@
-
+import about from '../../assets/breadcrumb.jpg';
 
 const About = () => {
     return (
         <div>
-            <h1>About</h1>
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src={about} className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
