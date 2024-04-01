c6312b9e44a2733c7aa84738ebcfbdeda1aad531
liton61 <litonofficial61@gmail.com>
2024-04-02 01:54:52 +0600

    products added to the shop page

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,7 +1,14 @@
 /* eslint-disable react/no-unescaped-entities */
+import { useEffect, useState } from "react";
 
 
 const Shop = () => {
+    const [shop, setShop] = useState([]);
+    useEffect(() => {
+        fetch('shop.json')
+            .then(res => res.json())
+            .then(data => setShop(data))
+    }, [])
     return (
         <div>
             <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/vd5Rgrp/service-01.jpg)' }}>
@@ -18,6 +25,22 @@ const Shop = () => {
                         <input className="input input-bordered focus:outline-none join-item" placeholder="Search Your Product" />
                         <button className="btn bg-green-900 text-yellow-200 join-item">Search <i className="fa-solid fa-magnifying-glass"></i></button>
                     </div>
+                </div>
+            </div>
+            <div className='lg:w-3/4 mx-auto lg:px-0 px-5 py-10'>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5'>
+                    {shop.map(shop => (<div key={shop._id} className="card card-compact bg-base-100 shadow-xl rounded-none">
+                        <figure><img className="h-36" src={shop.image} alt="" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{shop.title}</h2>
+                            <p className="font-medium text-gray-600">Price : {shop.price} Tk</p>
+                            <div className="card-actions justify-center gap-10">
+                                <button><i className="fa-solid fa-cart-plus text-xl text-green-700"></i></button>
+                                <button><i className="fa-solid fa-eye text-xl text-green-700"></i></button>
+                                <button><i className="fa-solid fa-heart text-xl text-green-700"></i></button>
+                            </div>
+                        </div>
+                    </div>))}
                 </div>
             </div>
         </div>
