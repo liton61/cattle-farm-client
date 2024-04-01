abe0ac6b027988d724b9c42e701fb2bed2ca90d0
liton61 <litonofficial61@gmail.com>
2024-04-02 00:47:40 +0600

    goat banner added

--- src/pages/Goat/Goat.jsx
+++ src/pages/Goat/Goat.jsx
@@ -1,9 +1,15 @@
+/* eslint-disable react/no-unescaped-entities */
 
 
 const Goat = () => {
     return (
-        <div>
-            <h1>Goat</h1>
+        <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/wdtm3NZ/Goat-Farming.jpg)' }}>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md bg-black bg-opacity-50 lg:p-10 md:p-10 p-3 rounded">
+                    <h1 className="mb-5 text-4xl font-bold">HAPPY FARMING</h1>
+                    <p className="mb-5 text-yellow-200">Farming is not just a livelihood, it's a legacy we sow today for future generations to reap !!!</p>
+                </div>
+            </div>
         </div>
     );
 };
