acbf201bdaa2377f8f3e11f84bbafbdb9d6b218c
liton61 <litonofficial61@gmail.com>
2024-04-02 01:15:21 +0600

    goat added to the goat page

--- src/pages/Cow/Cow.jsx
+++ src/pages/Cow/Cow.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/no-unescaped-entities */
-
 import { useEffect, useState } from "react";
 
 
--- src/pages/Goat/Goat.jsx
+++ src/pages/Goat/Goat.jsx
@@ -1,13 +1,39 @@
 /* eslint-disable react/no-unescaped-entities */
+import { useEffect, useState } from "react";
 
 
 const Goat = () => {
+    const [goat, setGoat] = useState([]);
+    useEffect(() => {
+        fetch('goat.json')
+            .then(res => res.json())
+            .then(data => setGoat(data))
+    }, [])
     return (
-        <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/wdtm3NZ/Goat-Farming.jpg)' }}>
-            <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md bg-black bg-opacity-50 lg:p-10 md:p-10 p-3 rounded">
-                    <h1 className="mb-5 text-4xl font-bold">HAPPY FARMING</h1>
-                    <p className="mb-5 text-yellow-200">Farming is not just a livelihood, it's a legacy we sow today for future generations to reap !!!</p>
+        <div>
+            <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/wdtm3NZ/Goat-Farming.jpg)' }}>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md bg-black bg-opacity-50 lg:p-10 md:p-10 p-3 rounded">
+                        <h1 className="mb-5 text-4xl font-bold">HAPPY FARMING</h1>
+                        <p className="mb-5 text-yellow-200">Farming is not just a livelihood, it's a legacy we sow today for future generations to reap !!!</p>
+                    </div>
+                </div>
+            </div>
+            <div className='lg:w-3/4 mx-auto lg:px-0 px-5 py-10'>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                    {goat.map(goat => (<div key={goat._id} className="card card-compact bg-base-100 shadow-xl rounded-none">
+                        <figure><img src={goat.image} alt="" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{goat.title}
+                                <div className="badge badge-secondary">Weight : {goat.weight} Kg</div>
+                            </h2>
+                            <p className="font-medium text-gray-600">Price : {goat.price} Tk</p>
+                            <p className="font-medium text-gray-600">Age : {goat.age} Months Tk</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn bg-green-900 hover:bg-green-800 text-yellow-200 w-full uppercase">Book Now</button>
+                            </div>
+                        </div>
+                    </div>))}
                 </div>
             </div>
         </div>
